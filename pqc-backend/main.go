cce1d7ca62a63d8cccb4146c0c34147927f2269f
Amaan Parvez <amaanparvez.cs@gmail.com>
2025-10-24 23:50:00 +0530

    renamed folder from crawler to scraper

--- pqc-backend/main.go
+++ pqc-backend/main.go
@@ -1,5 +1,16 @@
-package pqc_backend
+package main
+
+import "fmt"
 
 func main() {
-	
+	s := "hello"
+	runes := []rune(s)
+	i, j := 0, len(runes) - 1
+	for i < j {
+		runes[i], runes[j] = runes[j], runes[i]
+		i += 1
+		j -= 1
+	}
+	str := string(runes)
+	fmt.Print(str)
 }
--- scrape/scrapers/ibm_rss.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-ibm_rss_feed = requests.get("https://research.ibm.com/rss?fid=rss")
-
-print(type(ibm_rss_feed))
